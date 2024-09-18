5b66f146395035e72b7a10720eb1d2eb29d7d132
Craig Weston <craig@craigweston.ca>
2024-09-17 22:28:19 -0400

    updates for arduino pro mini

--- DCR/vfo_si5351/vfo_si5351.ino
+++ DCR/vfo_si5351/vfo_si5351.ino
@@ -3,16 +3,16 @@
 #include <Rotary.h>
 #include <si5351.h>
 
-#define ENCODER_PIN_A 8
-#define ENCODER_PIN_B 9
-#define ENCODER_SWITCH_PIN 10 
+const uint8_t encoderPinA = 8;
+const uint8_t encoderPinB =  9;
+const uint8_t encoderSwitchPin = 7;
 
-#define SI5351_CAL  127850 
-#define MIN_STEP 1
-#define MAX_STEP 10000
+const int32_t Si5351Cal = 127850;
+const uint16_t minStep = 1;
+const uint16_t maxStep = 10000;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
-Rotary r = Rotary(ENCODER_PIN_A, ENCODER_PIN_B);
+Rotary r = Rotary(encoderPinA, encoderPinB);
 Si5351 si5351;
 
 volatile uint32_t freq = 7200000;
@@ -28,37 +28,34 @@ void setup() {
 }
 
 void loop() {
-  int switchState = digitalRead(ENCODER_SWITCH_PIN);
+  int switchState = digitalRead(encoderSwitchPin);
   if (switchState == LOW) {
     nextStep();
     delay(300);
   }
   if (lastFreq != freq) {
     lastFreq = freq;
-    Serial.println("Setting Si5351...");
     setSi5351();
     displayFreq();
   }
 }
 
 void setupEncoder() {
-  pinMode(ENCODER_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(encoderSwitchPin, INPUT_PULLUP);
   r.begin();
   PCICR |= (1 << PCIE0);
-  PCMSK0 |= (1 << PCINT4) | (1 << PCINT5);
+  PCMSK0 |= (1 << PCINT0) | (1 << PCINT1);
   sei();
 }
 
 void setupSi5351() {
   bool i2cFound  = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
-  if(!i2cFound) {
-    Serial.println("Device not found on I2C bus!");
-  } else {
+  if(i2cFound) {
     si5351.output_enable(SI5351_CLK0, 1);
     si5351.output_enable(SI5351_CLK1, 0);
     si5351.output_enable(SI5351_CLK2, 0);
 
-    si5351.set_correction(SI5351_CAL, SI5351_PLL_INPUT_XO);
+    si5351.set_correction(Si5351Cal, SI5351_PLL_INPUT_XO);
     si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_2MA);
 
     setSi5351();
@@ -75,7 +72,9 @@ void setupSi5351() {
     Serial.print(si5351.dev_status.LOS);
     Serial.print("  REVID: ");
     Serial.println(si5351.dev_status.REVID);
-  }
+  } else {
+    Serial.println("Device not found on I2C bus!");
+  } 
 }
 
 void setSi5351() {
@@ -86,13 +85,13 @@ void setupDisplay() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);                   
-
   lcd.print("HELLO");  
   delay(2000);
   lcd.clear();
 }
 
 ISR(PCINT0_vect) {
+  Serial.println("INTERUPT");
   unsigned char result = r.process();
   if (result == DIR_CW) {
     setFreq(1);
@@ -107,21 +106,19 @@ void setFreq(int dir) {
 }
 
 void displayFreq() {
-  uint32_t mhz = freq / 1000000;
-  uint32_t khz = (freq % 1000000) / 1000;
-  uint32_t hz = freq % 1000;
-
+  uint16_t mhz = freq / 1000000;
+  uint16_t khz = freq % 1000000 / 1000;
+  uint16_t hz = freq % 1000;
   char buffer[11];
-  sprintf(buffer, "%02d.%03d.%03d", mhz, khz, hz);
-
+  snprintf(buffer, sizeof(buffer), "%02d.%03d.%03d", mhz, khz, hz);
   lcd.setCursor(0, 0);                   
   lcd.print(buffer);
 }
 
 void nextStep() {
   freqStep *= 10;
-  if (freqStep > MAX_STEP) {
-    freqStep = MIN_STEP;
+  if (freqStep > maxStep) {
+    freqStep = minStep;
   }
   if (freqStep < 1000) {
     freq -= freq % 1000;
